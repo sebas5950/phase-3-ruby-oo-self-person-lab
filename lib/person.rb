2a26e632a1c76b7ed1ab6e16d3ff3e93ca69e4dc
Alex Guanipatin <sebas5950@gmail.com>
2022-06-30 09:56:46 -0400

    commit tree de2eb45d9907e9f1ffd0a1c2f68b40363d23a408

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,33 @@
-# your code goes here
+require 'pry'
+class Person 
+
+    attr_reader :name, :bank_account
+
+    attr_writer :bank_account
+
+def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene <= 10 ? hygiene : @hygiene
+    end
+
+    def hygiene
+        @hygiene
+    end
+    
+end
+
+binding.pry
